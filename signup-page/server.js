636b37a25512cc74164b327d3313d23b5e063784
saran-9112000 <101337492+saran-9112000@users.noreply.github.com>
2022-03-11 12:45:16 +0530

    task 1 and 2 completed

---
+++ signup-page/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3001,
+
+
+  User = require('./api/models/userModel'),
+  bodyParser = require('body-parser'),
+  jsonwebtoken = require("jsonwebtoken");
+
+const mongoose = require('mongoose');
+const option = {
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30000
+};
+
+mongoose.connect('mongodb+srv://saran_1:saransatheesh@cluster0.f65ey.mongodb.net/test').then(function(){
+  console.log('connected to db')
+    //connected successfully
+}, function(err) {
+  console.log('unable to connect db')
+    //err handle
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+var routes = require('./api/route/userRoute');
+routes(app);
+
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
+
+app.listen(port);
+
+console.log(' RESTful API server started on: ' + port);
+
+module.exports = app;
